8c3a3ee84a0b5679f20f93c12293db1c2f083620
kevinkim1030 <kevin.kim1030@gmail.com>
2021-07-15 15:40:11 -0400

    auth created as listener

--- src/App.js
+++ src/App.js
@@ -4,19 +4,17 @@ import * as ROUTES from './constants/routes';
 
 const SignUp = lazy(() => import('./pages/sign-up'));
 const Login = lazy(() => import('./pages/login'));
+const Dashboard = lazy(() => import('./pages/dashboard'));
 const NotFound = lazy(() => import('./pages/not-found'));
 
 export default function App() {
-  function showOnGithub(){
-    return true
-  }
-  
   return (
     <Router>
       <Suspense fallback={<p>Loading...</p>}>
         <Switch>
           <Route path={ROUTES.LOGIN} component={Login} />
           <Route path={ROUTES.SIGN_UP} component={SignUp} />
+          <Route path={ROUTES.DASHBOARD} component={Dashboard} />
           <Route component={NotFound} />
         </Switch>
       </Suspense>
