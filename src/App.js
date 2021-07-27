e3c259058a0fd6b88ebdae6f9f89797c14241100
kevinkim1030 <kevin.kim1030@gmail.com>
2021-07-27 13:34:35 -0400

    created protected routes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 import useAuthListener from './hooks/use-auth-listener';
 import UserContext from './context/user';
+import ProtectedRoute from './helpers/protected-route';
+import IsUserLoggedIn from './helpers/is-user-logged-in';
 
 const SignUp = lazy(() => import('./pages/sign-up'));
 const Login = lazy(() => import('./pages/login'));
 const Dashboard = lazy(() => import('./pages/dashboard'));
+const Profile = lazy(() => import('./pages/profile'));
 const NotFound = lazy(() => import('./pages/not-found'));
 
 export default function App() {
@@ -16,9 +19,16 @@ export default function App() {
       <Router>
         <Suspense fallback={<p>Loading...</p>}>
           <Switch>
-            <Route path={ROUTES.LOGIN} component={Login} />
-            <Route path={ROUTES.SIGN_UP} component={SignUp} />
-            <Route path={ROUTES.DASHBOARD} component={Dashboard} />
+            <IsUserLoggedIn user={user} loggedInPath={ROUTES.DASHBOARD} path={ROUTES.LOGIN}>
+              <Login />
+            </IsUserLoggedIn>
+            <IsUserLoggedIn user={user} loggedInPath={ROUTES.DASHBOARD} path={ROUTES.SIGN_UP}>
+              <SignUp />
+            </IsUserLoggedIn>
+            <Route path={ROUTES.PROFILE} component={Profile} />
+            <ProtectedRoute user={user} path={ROUTES.DASHBOARD} exact>
+              <Dashboard />
+            </ProtectedRoute>
             <Route component={NotFound} />
           </Switch>
         </Suspense>
--- src/components/post/comments.js
+++ src/components/post/comments.js
@@ -6,15 +6,30 @@ import AddComment from './add-comment';
 
 export default function Comments({ docId, comments: allComments, posted, commentInput }) {
   const [comments, setComments] = useState(allComments);
+  const [commentsSlice, setCommentsSlice] = useState(3);
+
+  const showNextComments = () => {
+    setCommentsSlice(commentsSlice + 3);
+  };
+
   return (
     <>
       <div className="p-4 pt-1 pb-4">
-        {comments.length >= 3 && (
-          <p className="text-sm text-gray-base mb-1 cursor-pointer">
-            View all {comments.length} comments
-          </p>
+        {comments.length >= 3 && commentsSlice < comments.length && (
+          <button
+            className="text-sm text-gray-base mb-1 cursor-pointer focus:outline-none"
+            type="button"
+            onClick={showNextComments}
+            onKeyDown={(event) => {
+              if (event.key === 'Enter') {
+                showNextComments();
+              }
+            }}
+          >
+            View more comments
+          </button>
         )}
-        {comments.slice(0, 3).map((item) => (
+        {comments.slice(0, commentsSlice).map((item) => (
           <p key={`${item.comment}-${item.displayName}`} className="mb-1">
             <Link to={`/p/${item.displayName}`}>
               <span className="mr-1 font-bold">{item.displayName}</span>
