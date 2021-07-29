7f2bc4e317fac14023cb00116ce620f7470214b1
kevinkim1030 <kevin.kim1030@gmail.com>
2021-07-29 19:14:28 -0400

    updated npm@latest

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,16 +5,25 @@ import 'firebase/auth';
 // here I want to import the seed file
 // import { seedDatabase } from '../seed';
 
-const config = {
+// const config = {
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APP_ID
+// };
+
+const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  appId: REACT_APP_APP_ID
 };
 
-const firebase = Firebase.initializeApp(config);
+const firebase = Firebase.initializeApp(firebaseConfig);
 const { FieldValue } = Firebase.firestore;
 
 // here is where I want to call the seed file (only ONCE)
